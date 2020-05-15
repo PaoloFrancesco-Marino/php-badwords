bd2de0018ac89e580bbac3ad6e6ee0c9c26cc3e7
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-05-15 14:57:24 +0200

    welcome php

--- index.php
+++ index.php
@@ -6,6 +6,40 @@
     <title>Welcome PHP</title>
 </head>
 <body>
+
+    <!-- PHP -->
+    <?php
+    /***
+     * Descrizione:
+        Creare una variabile con un paragrafo di testo.
+        Visualizzare a schermo il paragrafo
+        Visualizzare a schermo la relativa lunghezza del paragrafo
+        Sostituire tutte le ricorrenze, nel paragrafo creato, della badword passata in GET (query string) con tre * (asterschi).
+     */
+
+    //  paragrafo
+    $paraghrap = "Questo è il testo iniziale. Benvenuto PHP, lorem ipsum dolor sit amet consectetur adipisicing elit. Autem facilis dolor quas. Velit deserunt corrupti quia, dolore facilis odit, mollitia earum, itaque non accusantium maiores vel rerum error sapiente ipsam!";
+
+    // lunghezza paragrafo
+    $lunghezza_stringa = strlen($paraghrap);
+
+    //stampa direttamente da php
+    // echo "la lunghezza del testo è:" . ' ' . $lunghezza_stringa;
+
+    // usiamo il get
+    $word = $_GET["badword"];
+    // sostituiamo
+    $replace = str_replace($word, "***", $paraghrap);
+    ?>
+
+    <header>
+        <p><?php  echo $paraghrap  ?></p>
+        <p>La lunghezza del paragrafo è: <?php echo $lunghezza_stringa ?></p>
+        <p>Queta è la parola che vuoi cambiare: <strong><?php echo $word ?></strong></p>
+        <p>Questo è il testo ristampato: <?php echo $replace ?></p>
+    </header>
+    
+
     
 </body>
 </html>
